1d97cfa9c6718a4a702de72d8768c8fd12d4c1d6
leo-72 <fihdanps@gmail.com>
2023-08-20 21:39:04 +0700

    Add Animation

---
+++ .history/dist/js/script_20230820213148.js
@@ -0,0 +1,19 @@
+// Navbar Line
+const navbarLine = document.querySelector("#navbarline");
+const navMenu = document.querySelector("#nav-menu");
+
+navbarLine.addEventListener("click", function () {
+  navbarLine.classList.toggle("nl-active");
+  navMenu.classList.toggle("hidden");
+});
+
+// Navbar Fixed
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector("header");
+  navbar.classList.toggle("navbar-fixed", window.scrollY > 0);
+});
+
+
+// Close Navbar 
+window.addEventListener("click", function () {
+  if (e.target)
---
+++ .history/dist/js/script_20230820213337.js
@@ -0,0 +1,19 @@
+// Navbar Line
+const navbarLine = document.querySelector("#navbarline");
+const navMenu = document.querySelector("#nav-menu");
+
+navbarLine.addEventListener("click", function () {
+});
+
+// Navbar Fixed
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector("header");
+  navbar.classList.toggle("navbar-fixed", window.scrollY > 0);
+});
+
+
+// Close Navbar 
+window.addEventListener("click", function (e) {
+  if (e.target !=navbarLine && e.target != navMenu){
+    
+  }});
